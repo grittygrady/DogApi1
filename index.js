80f2536f9e40d2c710215f29c34e8ec2582c196c
Christopher Grady <grittygrady@gmail.com>
2020-06-22 10:02:14 -0700

    Updated index.js for console logging

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ function getDogPics() {
     .catch(error => alert("Something went wrong, please try again"));
 }
 
+function logResponse() {
+  let numOfDogs = $('#quantity').val();
+  fetch(`https://dog.ceo/api/breeds/image/random/${numOfDogs}`)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson.message))
+    .catch(error => alert("Something went wrong, please try again"));
+}
+
 function displayDogs(responseJson) {
     $(".results").append(`<h2>Here's your dogs!</h2>`)
   for (let i = 0; i < responseJson.message.length; i++) {
@@ -25,6 +33,7 @@ function formListener() {
     let numOfDogs = $('#quantity').val();
     console.log(numOfDogs);
     getDogPics();
+    logResponse();
   });
 }
 
